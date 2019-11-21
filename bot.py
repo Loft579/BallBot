245f32cd0c2602744a41686e216ffe784d6ad6ea
Loft579 <51039400+Loft579@users.noreply.github.com>
2019-11-21 02:36:34 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,17 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-import os
 
 client = discord.Client()
-bot = commands.Bot(command_prefix = "z_")
+bot = commands.Bot(command_prefix = "p")
 
 @bot.listen()
 async def on_ready():
-    pass
+    print("bot on")
     
 @bot.command()
-async def nothing(ctx, *args):
-    pass
-
+async def ing(ctx, *args):
+    await ctx.author.voice.channel.connect()
+    
 TOKEN = os.environ['BOT_TOKEN']
 bot.run(TOKEN)
