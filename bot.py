0892b356795541a0ca772e1a0e7dfb4c5387c997
Loft579 <51039400+Loft579@users.noreply.github.com>
2019-11-20 21:52:54 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import time
+
+client = discord.Client()
+bot = commands.Bot(command_prefix = "z_")
+
+@bot.listen()
+async def on_ready():
+    pass
+    
+@bot.command()
+async def nothing(ctx, *args):
+    pass
+
+TOKEN = process.env.BOT_TOKEN
+bot.run(TOKEN)
